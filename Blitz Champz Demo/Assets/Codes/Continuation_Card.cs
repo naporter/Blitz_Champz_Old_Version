1d5097892459ffe8cc6bd54d4bf1fc392183822e
caleb523 <caleb523@gmail.com>
2019-10-17 19:15:40 -0500

    Added check to verify that player had not just drawn a card. ie making sure one can't keep playing draw cards

--- Blitz Champz Demo/Assets/Codes/Continuation_Card.cs
+++ Blitz Champz Demo/Assets/Codes/Continuation_Card.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Continuation_Card : Card {
 	void Start () {
-		
 	}
 	protected override void Play() {
 		AdvanceTurn();
 	}
-	// Update is called once per frame
 	void Update () {
-		
 	}
 }
