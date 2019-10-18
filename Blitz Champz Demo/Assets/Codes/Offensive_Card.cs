ee3fb8135ae1e7434e654d1712ae84d818645277
caleb523 <caleb523@gmail.com>
2019-10-17 20:32:28 -0500

    Redid ordering system so that it is a pseudo-linked list

--- Blitz Champz Demo/Assets/Codes/Offensive_Card.cs
+++ Blitz Champz Demo/Assets/Codes/Offensive_Card.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Offensive_Card : Card {
-	
 	protected int value;
 	protected bool kick = false;
 	protected bool pass = false;
 	protected bool run = false;
-	void Start () {	
+	void Start() {
 	}
-
 	protected override void Play() {
+		//owner.field.;
 		owner.update_score(value);
 		AdvanceTurn();
 	}
-
 	protected void Remove() { //remove card from the field and discard it thus removing points from that player
 		owner.update_score((-1) * value);
 	}
--- Blitz Champz Demo/Assets/Codes/Table.cs
+++ Blitz Champz Demo/Assets/Codes/Table.cs
@@ -10,19 +10,21 @@ public class Table : MonoBehaviour
     public void Discard(GameObject card) {
         card.GetComponent<SpriteRenderer>().sortingOrder = discard.Count;
         discard.Add(card);
-
     }
+    public Card last_card;
     public Player pov_player;
     public Player player1;
     public Player player2;
     public Text p1;
     public Text p2;
     public Player current_player;
+    LinkedListNode<Player> current;
     // Start is called before the first frame update
-    public LinkedList<Player> order;
-    void Start()
-    {
-        Player[] players = {player1, player2};
+    public LinkedList<Player> order = new LinkedList<Player>();
+    void Start() {
+        order.AddLast(player1);
+        order.AddLast(player2);
+        current = order.First;
         current_player = player1;
         for (int i = 0; i < 5; i++) {
             current_player = player2;
@@ -34,6 +36,14 @@ public class Table : MonoBehaviour
     }
     public void AdvanceTurn(Player player) {
         Update_Scores();
+        if (last_card) {
+        }
+        current = current.Next ?? current.List.First;
+        current_player = current.Value;
+        current_player.draw();
+        current_player.drew = false;
+
+        /*
         if (player == player1) {
             current_player = player2;
             player2.draw();
@@ -43,14 +53,12 @@ public class Table : MonoBehaviour
             player1.draw();
             player1.drew = false;
         }
+        */
     }
     public void Update_Scores() {
         p1.text = (player1.score).ToString();
         p2.text = (player2.score).ToString();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
