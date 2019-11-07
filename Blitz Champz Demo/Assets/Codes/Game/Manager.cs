822d9a4bc309a0f9dcbe6781969de63358fd494c
caleb523 <caleb523@gmail.com>
2019-11-07 13:53:07 -0600

    Merged main menu with newest version of project. Redid the method for passing player count between scenes

---
+++ Blitz Champz Demo/Assets/Codes/Game/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Manager
+{
+    private static int player_count;
+    public static int PlayerCount {
+        get {
+            return player_count;
+        }
+        set {
+            player_count = value;
+        }
+    }
+}
