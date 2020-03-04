ef4a46d9567dce3cfcbac6174e2067add4b09113
Hunter Brickeen <hbrckeen@memphis.edu>
2020-03-04 17:59:06 -0600

    Continuation cards now play a sound when played

--- Blitz Champz Demo/Assets/Codes/Cards/Continuation/Five_Yard_Run.cs
+++ Blitz Champz Demo/Assets/Codes/Cards/Continuation/Five_Yard_Run.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Five_Yard_Run : Continuation_Card
 {
+    //Get the AudioSource for each Offensive card
+	private AudioSource source;
     void Start() {
     }
     protected override void Play() {
         owner.Draw();
+        //When the card is played, play the sound attached to it
+		source = GetComponent<AudioSource>();
+		source.Play();
         AdvanceTurn();
     }
 	public override void Show() {
