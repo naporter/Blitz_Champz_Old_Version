7c054d332b616fcc47f28057f1f595fb01bc60eb
HBrickeen <46763336+HBrickeen@users.noreply.github.com>
2020-04-08 23:44:40 -0500

    Pass Completion speed increased

--- Blitz Champz Demo/Assets/Codes/Cards/Continuation/Pass_Completion.cs
+++ Blitz Champz Demo/Assets/Codes/Cards/Continuation/Pass_Completion.cs
@@ -7,7 +7,7 @@ public class Pass_Completion : Continuation_Card
     //Get the AudioSource for each Offensive card
 	private AudioSource source;
     //animation
-    public float speed = 0.5f;
+    public float speed = 2.5f;
     private Vector3 target;
     private Vector3 position;
     void Start() {
@@ -45,7 +45,7 @@ public class Pass_Completion : Continuation_Card
             transform.position = Vector3.MoveTowards(
                 transform.position,
                 target,
-                speed * Time.deltaTime);
+                speed);
             // Wait a frame and move again.
             yield return null;
         }
