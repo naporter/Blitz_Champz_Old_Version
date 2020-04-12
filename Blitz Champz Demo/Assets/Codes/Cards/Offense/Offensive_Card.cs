b3c1b8bc930bdd17beba868ca0ce57729b26fc1d
HBrickeen <46763336+HBrickeen@users.noreply.github.com>
2020-04-12 01:51:09 -0500

    Fixed Offensive Discard animation
    Defensive cards caused offensive cards to stay on the field face up, they now move to the discard pile

--- Blitz Champz Demo/Assets/Codes/Cards/Offense/Offensive_Card.cs
+++ Blitz Champz Demo/Assets/Codes/Cards/Offense/Offensive_Card.cs
@@ -9,6 +9,11 @@ public class Offensive_Card : Card {
 	protected bool run = false;
 	//Get the AudioSource for each Offensive card
 	private AudioSource source;
+
+	//test work under here
+	public float speed = 1f;
+    private Vector3 target;
+    private Vector3 position;
 	void Start() {
 	}
 	public bool GetKick() {
@@ -40,6 +45,10 @@ public class Offensive_Card : Card {
 	}
 	public void Remove() { //remove card from the field and discard it thus removing points from that player
 		owner.UpdateScore();
+		//test work under here
+		target = new Vector3(-1.45f, 0f, 0f);
+        position = gameObject.transform.position;
+		StartCoroutine(MoveTo());
 		Discard();
 	}
 	private void OnMouseUpAsButton() {
@@ -49,4 +58,22 @@ public class Offensive_Card : Card {
 	}
 	void Update () {
 	}
+
+	 IEnumerator MoveTo()
+    {
+       
+
+        // This looks unsafe, but Unity uses
+        // en epsilon when comparing vectors.
+        while (transform.position != target)
+        {
+            Debug.Log("Got to 4 loop");
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target,
+                speed);
+            // Wait a frame and move again.
+            yield return null;
+        }
+    }
 }
